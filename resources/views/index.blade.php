390b6f20ff7b12684b674d9a89d4f4e6c91c598d
takuya-ya <161791354+takuya-ya@users.noreply.github.com>
2025-05-08 04:57:33 +0000

    initial commit

---
+++ resources/views/index.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+  <link rel="stylesheet" href="{{ url('style.css')}}">
+</head>
+<body>
+    <div class="container">
+        <h1>Posts</h1>
+        <ul>
+            <li>Post</li>
+            <li>Post</li>
+            <li>Post</li>
+        </ul>
+    </div>
+</body>
+</html>
